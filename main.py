85de6d693b8d3c8e8a4f807452521781d0961e57
zanardo05 <gabrielzanardo05@outlook.com>
2026-05-17 00:19:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 
+import secrets
+import hashlib
+import base64
+
 app = FastAPI()
 
 app.add_middleware(
@@ -12,9 +16,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-FRONTEND_URL = "https://mz-dolar.vercel.app"
+CLIENT_ID = "33i32GbNdYrf99M2AM24V"
 
-APP_ID = "333299224"
+FRONTEND_URL = (
+    "https://mz-dolar.vercel.app"
+)
+
+REDIRECT_URI = (
+    "https://mz-dolar-python.onrender.com/auth/callback"
+)
+
+# PKCE
+def generate_pkce():
+
+    code_verifier = (
+        secrets.token_urlsafe(64)
+    )
+
+    challenge = hashlib.sha256(
+        code_verifier.encode()
+    ).digest()
+
+    code_challenge = base64.urlsafe_b64encode(
+        challenge
+    ).decode().replace("=", "")
+
+    return (
+        code_verifier,
+        code_challenge
+    )
 
 @app.get("/")
 def home():
@@ -23,42 +53,52 @@ def home():
         "status": "online"
     }
 
-# LOGIN DERIV
+# LOGIN
 @app.get("/auth/login")
-def login_deriv():
+def login():
 
-    redirect_uri = (
-        "https://mz-dolar-python.onrender.com/auth/callback"
+    state =
+        secrets.token_urlsafe(16)
+
+    (
+        code_verifier,
+        code_challenge
+    ) = generate_pkce()
+
+    auth_url = (
+        "https://auth.deriv.com/oauth2/auth"
+        f"?response_type=code"
+        f"&client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URI}"
+        f"&scope=trade account_manage"
+        f"&state={state}"
+        f"&code_challenge={code_challenge}"
+        f"&code_challenge_method=S256"
     )
 
-    oauth_url = (
-        f"https://oauth.deriv.com/oauth2/authorize"
-        f"?app_id={APP_ID}"
-        f"&redirect_uri={redirect_uri}"
+    return RedirectResponse(
+        auth_url
     )
-
-    print("LOGIN URL:", oauth_url)
-
-    return RedirectResponse(oauth_url)
 
 # CALLBACK
 @app.get("/auth/callback")
-def callback(code: str = None):
+def callback(
+    code: str = None,
+    state: str = None
+):
 
     print("CODE:", code)
 
-    # DEBUG
     if not code:
 
         return {
             "erro": "OAuth code não encontrado"
         }
 
-    # volta para frontend
     redirect = (
         f"{FRONTEND_URL}/#/dashboard?code={code}"
     )
 
-    print("REDIRECT:", redirect)
-
-    return RedirectResponse(redirect)
+    return RedirectResponse(
+        redirect
+    )
